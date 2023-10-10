e09520b3f5d22573268ae04313e5428e8d63f129
amit-shah-cb <amit.shah@coinbase.com>
2023-10-10 16:01:44 -0500

    enable calls to mint

--- app/page.tsx
+++ app/page.tsx
@@ -5,31 +5,24 @@ import {base58 } from '@scure/base';
 import { Environment, Text3D,Center,Text, OrbitControls,useGLTF,Stats,Circle, MeshDistortMaterial} from '@react-three/drei'
 import { Canvas, useFrame, useThree} from '@react-three/fiber'
 import { Bloom, DepthOfField, EffectComposer, Noise, Scanline, Vignette,Grid ,ChromaticAberration} from '@react-three/postprocessing'
-import { materialOpacity } from 'three/examples/jsm/nodes/Nodes.js';
-
+import { materialOpacity } from 'three/examples/jsm/nodes/Nodes.js'
+import { Glitch } from '@react-three/postprocessing'
+import { GlitchMode } from 'postprocessing'
 //import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
-function Model() {
+function Model({setLoaded}:any) {
   const {camera, size:{width,height}} = useThree()
   const gltf = useGLTF('/david_head/scene.gltf')
-  const box = new THREE.Box3().setFromObject(gltf.scene);
-    // const boxSize = box.getSize(new THREE.Vector3()).length();
-    // const boxCenter = box.getCenter(new THREE.Vector3());
-    // camera.zoom = 
-    //   width/ (1.5*(box.max.x - box.min.x));
-  camera.zoom = width/ (1.*(10));       
+  //NOTE dynamic zoom which we wont currently support
+  // const box = new THREE.Box3().setFromObject(gltf.scene);
+  // const boxSize = box.getSize(new THREE.Vector3()).length();
+  // const boxCenter = box.getCenter(new THREE.Vector3());
+  // camera.zoom = 
+  //   width/ (1.5*(box.max.x - box.min.x));
+  camera.zoom = width/ (1.*(10));
+  setLoaded(true)       
   return <primitive object={gltf.scene} scale={.05} position={[0,-10/2,0]} />
 }
-
-const Box = () => {
-  return<></>
-  // return (
-  //   <mesh rotation-x={0.} rotation-z={0.2} position={[0,0,-5.5]}>
-  //     <planeGeometry args={[8, 6]} />
-  //     <meshToonMaterial color={"black"} />
-  //   </mesh>
-  // );
-};
 
 const Box2 = () => {
   return (
@@ -40,16 +33,6 @@ const Box2 = () => {
   );
 };
 
-function Scene() {
-  //const colorMap = useLoader(TextureLoader, 'bg.png')
-  return (
-    <>
-      <ambientLight intensity={0} />
-      <directionalLight />     
-    </>
-  )
-}
-
 type command = {
   command: string
   targetUrl: string
@@ -57,63 +40,78 @@ type command = {
 
 export default function Home() {
    const [status, updateStatus] = useState("PROCESSING")
-   const [box, setBox] = useState(null)
-  // const [data, setData] = useState(null)
-  // const [address,setAddress] = useState(null)
+   const [isLoaded, setLoaded] = useState(false)
+    const [address,setAddress] = useState(null)
   
-  // useEffect(() => {
-  //   const initEth = async()=>{
-  //     if((window as any).ethereum){
-  //       try {
-  //           const accounts = await (window as any).ethereum.request({
-  //               method: "eth_requestAccounts",
-  //           });
-  //           setAddress(accounts[0])
-  //       } catch (error) {
-  //           console.log(error)
-  //       }
-  //     }else{
-  //       console.error("window.ethereum context unavailable")
-  //     }
-  //   }
+  useEffect(() => {
+    const initEth = async()=>{
+      if((window as any).ethereum){
+        try {
+            const accounts = await (window as any).ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            setAddress(accounts[0])
+        } catch (error) {
+            console.log(error)
+        }
+      }else{
+        console.error("window.ethereum context unavailable")
+        updateStatus("ERROR")
+      }
+    }
     
-  //   initEth()
-  // }, [])
+    initEth()
+  }, [])
 
-  // useEffect(()=>{
-  //   if (address){
-  //     const queryParameters = new URLSearchParams(window.location.search)
-  //     const base58data = queryParameters.get("data") 
-  //     // var commandParamsE = base58.encode(Buffer.from(JSON.stringify({command: "cb_marketing_q4", targetUrl: 'https://api.wallet.coinbase.com/rpc/v2/bot/mint'})))
-  //     // console.log("encoded data",commandParamsE)
-  //     if (!base58data){
-  //       console.error("no data received")
-  //     }
-  //     var commandParams = JSON.parse(Buffer.from(base58.decode(base58data as string)).toString()) as command
-  //     console.log("decoded data",commandParams)
+  useEffect(()=>{
+    if (address && isLoaded){
+      const queryParameters = new URLSearchParams(window.location.search)
+      const base58data = queryParameters.get("data") 
+      // var commandParamsE = base58.encode(Buffer.from(JSON.stringify({command: "cb_marketing_q4", targetUrl: 'https://api.wallet.coinbase.com/rpc/v2/bot/mint'})))
+      // console.log("encoded data",commandParamsE)
+      if (!base58data){
+        updateStatus("ERROR")
+        console.error("no data received")
+      }
+      try{
+      var commandParams = JSON.parse(Buffer.from(base58.decode(base58data as string)).toString()) as command
+      
+      console.log("decoded data",commandParams)
+      
+      const fetchData = async () => {
+        console.log("query with address",address);
+        const response = await fetch('/api/relay',{
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({command: commandParams.command, userAddress: address, targetUrl: commandParams.targetUrl})
+        })
+        if (!response.ok) {
+          console.error(`HTTP error! status: ${response.status}`)
+          updateStatus("ERROR")
+        }
+        const result = await response.json()
+        console.log(result)
+        updateStatus(result.result);      
+      } 
 
-  //     const fetchData = async () => {
-  //       console.log("query with address",address);
-  //       const response = await fetch('/api/relay',{
-  //         method: 'POST',
-  //         headers: {
-  //           'Accept': 'application/json',
-  //           'Content-Type': 'application/json'
-  //         },
-  //         body: JSON.stringify({command: commandParams.command, userAddress: address, targetUrl: commandParams.targetUrl})
-  //       })
-  //       if (!response.ok) {
-  //         throw new Error(`HTTP error! status: ${response.status}`)
-  //       }
-  //       const result = await response.json()
-  //       setData(result)      
-  //     } 
-  //     fetchData().catch((e) => {
-  //       // handle the error as needed
-  //       console.error('An error occurred while fetching the data: ', e)
-  //     })
-  //   }
-  // },[address])
+      const timer = setTimeout(() => {
+        fetchData().catch((e) => {
+          updateStatus("TIMEOUT")
+          console.error('An error occurred while fetching the data: ', e)
+        })
+      }, 500);
+      return () => clearTimeout(timer);
+    }catch(e){
+      console.error(e)
+      updateStatus("ERROR")
+    }
+
+  }
+    
+  },[address,isLoaded])
 
   
   return (
@@ -122,10 +120,18 @@ export default function Home() {
        <Canvas orthographic shadows>
        <ambientLight intensity={2} color={"white"}/>
        <pointLight position={[1, 1, 1]} color={"blue"} intensity={12}/>
-      <directionalLight position={[10, 10, 10]} /> 
+        <directionalLight position={[10, 10, 10]} /> 
        <Suspense fallback={null}>    
-       <group>    
-          <Model />    
+       <group>
+          <group position-z={3.2} position-y={6.5}>
+          <Center bottom>
+            <Text3D font={'/gaming.json'} size={1} >            
+              AIRDROP
+              <meshBasicMaterial color={"white"} />
+            </Text3D>      
+          </Center>
+          </group>    
+          <Model setLoaded={setLoaded} />    
           <Box2 />
           {/* <group position={[.5,2.1,2.2]}>           
             <Center>           
@@ -145,7 +151,8 @@ export default function Home() {
               </Text>                     
             </Center>            
           </group>
-          <group position-z={4.2} position-y={-1}>
+          <group position-z={4.2} position-y={-2.}>
+          
           <Center bottom>
             <Text3D font={'/gaming.json'} size={.6} >            
               [ {status} ]
@@ -154,22 +161,20 @@ export default function Home() {
           </Center>
           </group>
         </group>
-       
-        </Suspense>
-        <Box />
-        
+        </Suspense>        
       
         <EffectComposer>
-        
-        {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
-        {/* <Grid scale={.1} /> */}
         <ChromaticAberration      
           // @ts-expect-error: Let's ignore a compile error  
           offset={[0.01, 0.002]} // color offset
         />
         <Bloom luminanceThreshold={0.1} luminanceSmoothing={0.9} height={500} />
+       
+        {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
+        {/* <Grid scale={.1} /> */}
         
-        {/* <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}        
+        
+        {/* <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}                
       </EffectComposer>
       <OrbitControls target={[0, 0, 0]} />
       <axesHelper args={[5]} />
